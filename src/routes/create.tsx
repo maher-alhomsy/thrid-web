24f3b13eaa749c4480f8d11b75beb09b4db81ada
maher-alhomsy <maheralhomsy2002@gmail.com>
2025-08-27 05:24:26 +0300

    add ConnectButton component

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -1,9 +1,31 @@
+import { ConnectButton } from 'thirdweb/react';
+import { createWallet } from 'thirdweb/wallets';
+import { abstractTestnet, defineChain } from 'thirdweb/chains';
+
+import { client } from '@/lib/thrid-web';
 import CreateForm from '@/components/create/form';
+
+const UNICHAIN_SEPOLIA = defineChain(1301);
 
 const Create = () => {
   return (
     <div className="min-h-screen bg-screen px-4">
       <CreateForm />
+
+      <div className="mt-4">
+        <ConnectButton
+          client={client}
+          chain={UNICHAIN_SEPOLIA}
+          switchButton={{ label: 'Switch' }}
+          chains={[UNICHAIN_SEPOLIA, abstractTestnet]}
+          autoConnect={true}
+          showAllWallets={false}
+          wallets={[
+            createWallet('io.metamask'),
+            createWallet('com.trustwallet.app'),
+          ]}
+        />
+      </div>
     </div>
   );
 };
